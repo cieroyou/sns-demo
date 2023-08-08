e9abc27df6fbb20441ac2a3ad88e797be5d3ee96
sera <cieroyou1@gmail.com>
2023-08-09 00:01:33 +0900

    게시글 데일리 조회(데일리 게시글 개수) API 구현 * controller, service, db read 구현 * Jpql, Projection, GroupBy 쿼리를 이용한 데일리 조회 구현

--- src/main/java/com/sera/snsdemo/domain/post/repository/PostRepository.java
+++ src/main/java/com/sera/snsdemo/domain/post/repository/PostRepository.java
@@ -1,10 +1,35 @@
 package com.sera.snsdemo.domain.post.repository;
 
+import com.sera.snsdemo.domain.post.dto.DailyPostCount;
 import com.sera.snsdemo.domain.post.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
-//    Optional<Post> findById(Long id);
-
+    /**
+     * DailyPostCount 가 record 일 때
+     * "SELECT new com.sera.snsdemo.domain.post.dto.DailyPostCount("
+     * + "p.memberId as memberId, p.createdDate as date, count(p.id) as postCount) "
+     * + "FROM Post p "
+     * + "WHERE memberId = :memberId AND createdDate BETWEEN :firstDate AND :lastDate "
+     * + "GROUP BY p.memberId, p.createdDate"
+     * DailyPostCount 가 interface 일 때
+     * "SELECT "
+     * + "p.memberId as memberId, p.createdDate as date, count(p.id) as postCount "
+     * + "FROM Post p "
+     * + "WHERE memberId = :memberId AND createdDate BETWEEN :firstDate AND :lastDate "
+     * + "GROUP BY p.memberId, p.createdDate"
+     */
+    @Query(
+            value = "SELECT new com.sera.snsdemo.domain.post.dto.DailyPostCount("
+                    + "p.memberId as memberId, p.createdDate as date, count(p.id) as postCount) "
+                    + "FROM Post p "
+                    + "WHERE memberId = :memberId AND createdDate BETWEEN :firstDate AND :lastDate "
+                    + "GROUP BY p.memberId, p.createdDate"
+            , nativeQuery = false)
+    List<DailyPostCount> groupByCreateDate(Long memberId, LocalDate firstDate, LocalDate lastDate);
 }
 
---
+++ src/main/java/com/sera/snsdemo/domain/post/service/PostReadService.java
@@ -0,0 +1,20 @@
+package com.sera.snsdemo.domain.post.service;
+
+import com.sera.snsdemo.domain.post.dto.DailyPostCount;
+import com.sera.snsdemo.domain.post.dto.DailyPostCountRequest;
+import com.sera.snsdemo.domain.post.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class PostReadService {
+    private final PostRepository postRepository;
+
+    public List<DailyPostCount> getDailyPostCount(DailyPostCountRequest request) {
+        return postRepository.groupByCreateDate(
+                request.getMemberId(), request.getFirstDate(), request.getLastDate());
+    }
+}
