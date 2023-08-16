6c49c6b5f246b11423bd76d756169efb95527cbc
sera <cieroyou1@gmail.com>
2023-08-16 23:01:45 +0900

    좋아요 비즈니스로직 작성

--- src/main/java/com/sera/snsdemo/domain/post/repository/PostRepository.java
+++ src/main/java/com/sera/snsdemo/domain/post/repository/PostRepository.java
@@ -2,15 +2,20 @@ package com.sera.snsdemo.domain.post.repository;
 
 import com.sera.snsdemo.domain.post.dto.DailyPostCount;
 import com.sera.snsdemo.domain.post.entity.Post;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    Optional<Post> findById(Long id);
     /**
      * DailyPostCount 가 record 일 때
      * "SELECT new com.sera.snsdemo.domain.post.dto.DailyPostCount("
